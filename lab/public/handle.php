f6efed328561956afd5e188ae30508125e58a7ed
m.boldachyov <21971@adata.kz>
2022-10-08 17:54:32 +0600

    Lab6

---
+++ lab/public/handle.php
@@ -0,0 +1,14 @@
+<?php
+
+$name = $_POST['name'];
+$city = $_POST['city'];
+$creditCard = $_POST['creditCard']
+
+$fine =
+?>
+
+<html>
+<body>
+<h2>HELLO <?php echo $_POST['name']; ?></h2>
+</body>
+</html>
---
+++ lab/public/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+    <body>
+
+    <form action="handle.php" method="post">
+        <div class="box">
+            <h1>Buy Your Way to a Better Education</h1>
+            <p>Recent changes in University of Washington policy now allow us to offer grades for money</p>
+            <hr>
+            <h1>Five us your money</h1>
+            <p>Name</p>
+            <input name="name" type="email">
+
+            <p>Section</p>
+            <select name="city" id="cities">
+               <option value="Naples">Naples</option>
+               <option value="London">London</option>
+               <option value="Berlin">Berlin</option>
+               <option value="New York">New York</option>
+               <option value="Frattamaggiore">Frattamaggiore</option>
+            </select>
+
+            <p>Credit Card</p>
+            <input name="credit_card" type="text">
+            <p>Visa</p>
+            <input type="radio" id="javascript" name="fav_language" value="Visa">
+            <p>Mastercard</p>
+            <input type="radio" id="javascript" name="fav_language" value="MasterCard">
+            <button type="submit">Submit</button>
+
+        </div>
+    </form>
+
+    </body>
+</html>
